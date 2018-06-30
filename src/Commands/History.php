5776f39f52760812c33ba8203c466ff46c779dc7
bennetgallein <bennet@intranetproject.net>
2018-06-30 17:34:48 +0200

    phpDoc comments

--- src/Commands/History.php
+++ src/Commands/History.php
@@ -15,52 +15,100 @@ class History {
 
     private $instance;
 
+    /**
+     * History constructor.
+     * @param Ts3AudioBot $instance
+     */
     public function __construct(Ts3AudioBot $instance) {
         $this->instance = $instance;
     }
 
+    /**
+     * @return mixed
+     */
     public function add() {
         return $this->instance->request("history/add");
     }
 
+    /**
+     * @return mixed
+     */
     public function clean() {
         return $this->instance->request("history/clean");
     }
 
+    /**
+     * @return mixed
+     */
     public function cleanRemovedefective() {
         return $this->instance->request("history/clean/removedefective");
     }
 
+    /**
+     * @return mixed
+     */
     public function delete() {
         return $this->instance->request("history/delete");
     }
 
+    /**
+     * @return mixed
+     */
     public function historyFrom() {
         return $this->instance->request("history/from");
     }
 
+    /**
+     * @return mixed
+     */
     public function historyID() {
         return $this->instance->request("history/id");
     }
 
+    /**
+     * @return mixed
+     */
     public function last() {
         return $this->instance->request("history/last");
     }
 
+    /**
+     * @param int $count
+     * @return mixed
+     */
     public function getLast(int $count) {
         return $this->instance->request("history/last/" . $count);
     }
 
+    /**
+     * @param int $id
+     * @return mixed
+     */
     public function play(int $id) {
         return $this->instance->request("history/play/" . $id);
     }
 
+    /**
+     * @param int $id
+     * @param string $name
+     * @return mixed
+     */
     public function rename(int $id, string $name) {
         return $this->instance->request("history/rename/" . $id . "/" . $name);
     }
+
+    /**
+     * @param string $date
+     * @return mixed
+     */
     public function till(string $date) {
         return $this->instance->request("history/till/" . $date);
     }
+
+    /**
+     * @param string $filter
+     * @return mixed
+     */
     public function filterTitle(string $filter) {
         return $this->instance->request("history/title/" . $filter);
     }
--- src/Ts3CommandCaller.php
+++ src/Ts3CommandCaller.php
@@ -50,130 +50,226 @@ class Ts3CommandCaller {
         return $this->instance->request("bot/commander/on");
     }
 
+    /**
+     * @return mixed
+     */
     public function takeCommander() {
         return $this->instance->request("bot/commander/off");
     }
 
+    /**
+     * @return mixed
+     */
     public function come() {
         return $this->instance->request("bot/come");
     }
 
+    /**
+     * @return mixed
+     */
     public function connectTo() {
         return $this->instance->request("bot/connect/to");
     }
 
+    /**
+     * @return mixed
+     */
     public function connectNew() {
         return $this->instance->request("bot/connect/new");
     }
 
+    /**
+     * @return mixed
+     */
     public function info() {
         return $this->instance->request("bot/info");
     }
 
+    /**
+     * @return mixed
+     */
     public function list() {
         return $this->instance->rawRequest("bot/list");
     }
 
+    /**
+     * @return mixed
+     */
     public function move() {
         return $this->instance->request("bot/move");
     }
 
+    /**
+     * @return mixed
+     */
     public function name() {
         return $this->instance->request("bot/name");
     }
 
+    /**
+     * @return mixed
+     */
     public function badges() {
         return $this->instance->request("bot/badges");
     }
 
+    /**
+     * @return mixed
+     */
     public function save() {
         return $this->instance->request("bot/save");
     }
 
+    /**
+     * @return mixed
+     */
     public function setup() {
         return $this->instance->request("bot/setup");
     }
 
+    /**
+     * @param $botid
+     */
     public function use($botid) {
         $this->instance->botid = $botid;
     }
 
+    /**
+     * @return mixed
+     */
     public function clear() {
         return $this->instance->request("clear");
     }
 
+    /**
+     * @return mixed
+     */
     public function disconnect() {
         return $this->instance->request("disconnect");
     }
 
+    /**
+     * @return mixed
+     */
     public function eval() {
         return $this->instance->request("eval");
     }
 
+    /**
+     * @return mixed
+     */
     public function getmyid() {
         return $this->instance->request("getmy/id");
     }
 
+    /**
+     * @return mixed
+     */
     public function getmyuid() {
         return $this->instance->request("getmy/uid");
     }
 
+    /**
+     * @return mixed
+     */
     public function getmyname() {
         return $this->instance->request("getmy/name");
     }
 
+    /**
+     * @return mixed
+     */
     public function getmydbid() {
         return $this->instance->request("getmy/dbid");
     }
 
+    /**
+     * @return mixed
+     */
     public function getmychannel() {
         return $this->instance->request("getmy/channel");
     }
 
+    /**
+     * @return mixed
+     */
     public function getmyall() {
         return $this->instance->request("getmy/all");
     }
 
+    /**
+     * @return mixed
+     */
     public function getuserUIDbyID() {
         return $this->instance->request("getuser/uid/byid");
     }
 
+    /**
+     * @return mixed
+     */
     public function getuserNamebyID() {
         return $this->instance->request("getuser/name/byid");
     }
 
+    /**
+     * @return mixed
+     */
     public function getuserDBIDbyID() {
         return $this->instance->request("getuser/dbid/byid");
     }
 
+    /**
+     * @return mixed
+     */
     public function getuserChannelbyID() {
         return $this->instance->request("getuser/channel/byid");
     }
 
+    /**
+     * @return mixed
+     */
     public function getuserAllbyID() {
         return $this->instance->request("getuser/all/byid");
     }
 
+    /**
+     * @return mixed
+     */
     public function getuserIDbyName() {
         return $this->instance->request("getuser/id/byname");
     }
 
+    /**
+     * @return mixed
+     */
     public function getuserAllbyName() {
         return $this->instance->request("getuser/all/byname");
     }
 
+    /**
+     * @return mixed
+     */
     public function getUserNamebyDBID() {
         return $this->instance->request("getuser/name/bydbid");
     }
 
+    /**
+     * @return mixed
+     */
     public function getUserUIDbyDBID() {
         return $this->instance->request("getuser/uid/bydbid");
     }
 
+    /**
+     * @return mixed
+     */
     public function help() {
         return $this->instance->request("help");
     }
 
+    /**
+     * @return History
+     */
     public function history() {
         return new History($this->instance);
     }
